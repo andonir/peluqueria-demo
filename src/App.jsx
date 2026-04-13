47c33ece2698547fb61c303aafca3d75d8856d56
andonir <andoniroura@outlook.com>
2026-04-14 01:45:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,7 @@ function App() {
       <Route path="/" element={<Navigate to="client" replace />} />
       <Route path="client" element={<Landing />}></Route>
       <Route path="client/profile" element={<Profile />}></Route>
-      <Route path="client/appointment" element={<h1>OK APPOINTMENT</h1>} />
-      {/* <Route path="/client/appointment" element={<Cita />}></Route> */}
+      <Route path="client/appointment" element={<Cita />}></Route>
       <Route path="*" element={<Navigate to="client" replace />} />
 
       {/* ADMIN */}
