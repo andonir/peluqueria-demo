dae2288a0273b2128954ffc221dd7d70da0b4640
andonir <andoniroura@outlook.com>
2026-04-14 14:14:35 +0200

    commit

--- src/client/components/cita/Login.jsx
+++ src/client/components/cita/Login.jsx
@@ -2,13 +2,14 @@ import { Context } from "../../../common/context/Context";
 import { useContext } from "react";
 import { logIn } from "../../../common/supabase/functions";
 const LogIn = () => {
-  const { isRegisterActive, setIsRegisterActive,setCurrentStep } = useContext(Context);
+  const { isRegisterActive, setIsRegisterActive, setCurrentStep } =
+    useContext(Context);
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    logIn(email,password)
-    setCurrentStep(2)
+    logIn(email, password);
+    setCurrentStep(2);
   };
   const showRegister = () => {
     setIsRegisterActive(true);
@@ -17,18 +18,17 @@ const LogIn = () => {
     <>
       {!isRegisterActive && (
         <>
-        
-          <form id="login" onSubmit={(e)=>handleSubmit(e)}>
+          <form id="login" onSubmit={(e) => handleSubmit(e)}>
             <input type="text" name="email" placeholder="Correo" />
             <input type="password" name="password" placeholder="Contraseña" />
             <button type="submit">Iniciar sesión</button>
           </form>
-          <h4>
-            No tienes cuenta?{" "}
+          <div className="bottom">
+            <h4>No tienes cuenta?</h4>
             <button type="button" onClick={showRegister}>
               REGÍSTRATE AHORA
             </button>
-          </h4>
+          </div>
         </>
       )}
     </>
--- src/client/components/cita/Register.jsx
+++ src/client/components/cita/Register.jsx
@@ -1,40 +1,42 @@
-import { useContext } from "react"
-import { Context } from "../../../common/context/Context"
-import { register } from "../../../common/supabase/functions"
-const Register = ()=>{
-    const {isRegisterActive, setIsRegisterActive} = useContext(Context)
-    const handleSubmit = (e)=>{
-      e.preventDefault()
-      const name = e.target.name.value;
-      const surname = e.target.surname.value;
-      const email = e.target.email.value;
-      const phone = e.target.phone.value;
-      const password = e.target.password.value;
-      register(name,surname,email,phone,password)
-    }
-    const hideRegister = ()=>{
-        setIsRegisterActive(false)
-    }
-    return<>
-    {isRegisterActive && ( 
+import { useContext } from "react";
+import { Context } from "../../../common/context/Context";
+import { register } from "../../../common/supabase/functions";
+const Register = () => {
+  const { isRegisterActive, setIsRegisterActive } = useContext(Context);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const surname = e.target.surname.value;
+    const email = e.target.email.value;
+    const phone = e.target.phone.value;
+    const password = e.target.password.value;
+    register(name, surname, email, phone, password);
+  };
+  const hideRegister = () => {
+    setIsRegisterActive(false);
+  };
+  return (
+    <>
+      {isRegisterActive && (
         <>
-        <form id="register" onSubmit={(e)=>handleSubmit(e)}>
-        <input type="text" name="name" placeholder="name"/>
-        <input type="text" name="surname" placeholder="surname"/>
-        <input type="text" name="email" placeholder="Correo" />
-        <input type="tel" name="phone" placeholder="Teléfono"/>
-        <input type="password" name="password" placeholder="password" />
-        <button type="submit">Registrarse</button>
-  </form> 
-   <h4>
-            Ya tienes cuenta?{" "}
+          <form id="register" onSubmit={(e) => handleSubmit(e)}>
+            <input type="text" name="name" placeholder="Nombre" />
+            <input type="text" name="surname" placeholder="Apellido" />
+            <input type="text" name="email" placeholder="Correo" />
+            <input type="tel" name="phone" placeholder="Teléfono" />
+            <input type="password" name="password" placeholder="Contraseña" />
+            <button type="submit">Registrarse</button>
+          </form>
+          <div className="bottom">
+            <h4>Ya tienes cuenta? </h4>
             <button type="button" onClick={hideRegister}>
               INICIAR SESIÓN
             </button>
-          </h4>
-  </>
-  )} 
+          </div>
+        </>
+      )}
     </>
-}
+  );
+};
 
-export default Register
+export default Register;
